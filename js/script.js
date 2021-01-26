9958501438bf7b452065a9277d6aade133f01079
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-26 13:29:22 +0100

    Exercise done!

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+ el: '#root',
+ data:{
+  title : 'Attenzione , sono un titolo e mi chiamo Bob!',
+  imgSource : './img/cat.jpg' ,
+ },
+ methods:{
+ 
+
+ },
+});
+
+Vue.config.devtools = true;
